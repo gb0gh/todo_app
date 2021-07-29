1922e035ce473398a8ba53e3ca5a6aca0bfa6e23
gbogh <gboghmf@gmail.com>
2021-07-29 11:46:03 +0900

    map null

--- js/gotomap.js
+++ js/gotomap.js
@@ -3,6 +3,8 @@ const word = gotomap.split(',');
 const place = word[0];
 
 $('.gotomap').click(function(){
+    if(gotomap != null){
     const tap = window.open('https://www.google.co.kr/maps/search/'+ place, '_blank');
     tap.focus();
+    }
 });
